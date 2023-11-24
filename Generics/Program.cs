b97c5f7ffcf57beb03e0b11ab0e99ef4fd9b6625
Meesam <meesam.engineer@gmail.com>
2023-11-24 13:59:44 +0530

    Implement generics

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Generics");
+        int a = 10, b = 20;
+        Console.WriteLine($"a {a}  b {b}");
+        Swap<int>(ref a, ref b );
+        Console.WriteLine($"a {a}  b {b}");
+
+        string c = "Hello", d = "World";
+        Console.WriteLine($"c {c}  d {d}");
+        Swap<string>(ref c, ref d);
+        Console.WriteLine($"c {c}  d {d}");
+    }
+
+    static void Swap<T>(ref T a, ref T b)
+    { 
+       T temp = a;
+       a = b;
+       b = temp;
+    }
+}
